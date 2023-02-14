8a9158cee775d1c23542432ad92bc2dc0981927f
Vanderlei Bueno <vanderlei.llbueno@hotmail.com>
2023-02-14 11:19:10 -0300

    Catalog tests

---
+++ frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Catalog from "..";
+import history from "util/history";
+import { Router } from "react-router-dom";
+
+test('should render Catalog with products', () => {
+
+    render(
+        <Router history={history}>
+            <Catalog />
+        </Router>
+    );
+
+    expect(screen.getByText('Catálogo de produtos')).toBeInTheDocument();
+});
